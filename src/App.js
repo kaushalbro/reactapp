b12089c64a9c69290ce7a41f8b6024dc55885ef6
devil <devil@DevilSensai.netis.cc>
2023-06-26 12:17:12 +0545

    hello text

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React, { useState } from 'react'
 
 function App() {
-const [carName, setCarName] = useState("yati4");
+const [carName, setCarName] = useState("yati4vscode");
 
   return (
     <>
     <div>
          this is a car {carName}
     </div>
-    
+    <p>hello this is kaushal</p>
     
     </>
   )
